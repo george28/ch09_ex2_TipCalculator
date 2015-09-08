1d82cff1a2ad89bca3df4e8e7ec1b16a7144799c
unknown <660162328@RM218-25-153.coleman.local>
2015-09-08 10:10:56 -0700

    Initial commit

---
+++ app/src/main/java/com/hargis/tipcalculator/AboutFragment.java
@@ -0,0 +1,16 @@
+package com.hargis.tipcalculator;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+
+public class AboutFragment extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_about);
+    }
+
+}
